939eaa5c04c3953bf85a361d14f14a9f8cbb5fe0
Matti Jokitulppo <melonmanchan@gmail.com>
2015-09-05 13:19:03 +0300

    Add chapter 1 extra exercises

---
+++ chapter1/temp-rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main() {
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("%6s\t%3s\n", "celsius", "fahr");
+
+    while (celsius <= upper) {
+        fahr = celsius * (9.0/5.0) + 32.0;
+        printf("%6.1f\t%3.0f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
