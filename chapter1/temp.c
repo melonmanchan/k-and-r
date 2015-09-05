451a10fc0bdd0d1167809c14bcd2d5e6beaa2a2d
Matti Jokitulppo <melonmanchan@gmail.com>
2015-09-05 13:12:49 +0300

    Add headings to printf

--- chapter1/temp.c
+++ chapter1/temp.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
@@ -14,9 +14,11 @@ main() {
 
     fahr = lower;
 
+    printf("%3s\t%6s\n", "fahr", "celsius");
+
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = 5.0 * (fahr-32.0) / 9.0;
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
