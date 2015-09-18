2a6f1ae84ba32a6ee78eb386412254de98f85d04
Matti Jokitulppo <melonmanchan@gmail.com>
2015-09-18 19:15:40 +0300

    Add 1-10 external variable example

--- chapter1/1-10/external-longest.c
+++ chapter1/1-10/external-longest.c
@@ -10,7 +10,7 @@ int max;
 char line[MAXLINE];
 char longest[MAXLINE];
 
-int getline(void);
+int getnewline(void);
 void copy(void);
 
 int main() {
@@ -19,7 +19,7 @@ int main() {
     extern char longest[];
 
     max = 0;
-    while((len = getline()) > 0) {
+    while((len = getnewline()) > 0) {
         if (len > max) {
             max = len;
             copy();
@@ -31,18 +31,28 @@ int main() {
     }
 }
 
-int getline(void) {
+int getnewline(void) {
     int c, i;
     extern char line[];
 
     for(i = 0; i < MAXLINE - 1 && (c=getchar()) != EOF && c != '\n'; ++i) {
         line[i] = c;
+        }
         if (c == '\n') {
             line[i] = c;
             ++i;
         }
         line[i] = '\0';
         return i;
+}
 
+void copy(void) {
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+
+    while((longest[i] = line[i]) != '\0') {
+        ++i;
     }
 }
